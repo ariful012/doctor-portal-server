9767e42fd246e689f260d268d9f91d500fef4f66
ariful012 <ariful.diu.cse@gmail.com>
2022-09-17 20:50:52 +0600

    include some

--- index.js
+++ index.js
@@ -152,12 +152,6 @@ async function run() {
     });
 
     /**
-     * API Naming Convention
-     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
-     * app.get('/booking/:id') // get a specific booking
-     * app.post('/booking') // add a new booking
-     * app.patch('/booking/:id) //
-     * app.put('/booking/:id') // upsert ==> update (if exists) or insert (if doesn't exist)
      * app.delete('/booking/:id) //
      */
 
